d34e67c72cba018ddd248e1f10b8982ab69b9dfa
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-29 22:19:42 -0300

    feat: cmdline module typed

--- stubs/Scrapy/scrapy/cmdline.pyi
+++ stubs/Scrapy/scrapy/cmdline.pyi
@@ -1,13 +1,13 @@
 import argparse
-from _typeshed import Incomplete
 
 from scrapy.commands import ScrapyCommand as ScrapyCommand, ScrapyHelpFormatter as ScrapyHelpFormatter
 from scrapy.crawler import CrawlerProcess as CrawlerProcess
 from scrapy.exceptions import UsageError as UsageError
+from scrapy.settings import Settings
 from scrapy.utils.misc import walk_modules as walk_modules
 from scrapy.utils.project import get_project_settings as get_project_settings, inside_project as inside_project
 from scrapy.utils.python import garbage_collect as garbage_collect
 
 class ScrapyArgumentParser(argparse.ArgumentParser): ...
 
-def execute(argv: Incomplete | None = ..., settings: Incomplete | None = ...) -> None: ...
+def execute(argv: str | None = ..., settings: Settings | None = ...) -> None: ...
