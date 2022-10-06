ab3aa503741151dac659b82910f8ed058bf171aa
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-10-06 19:58:31 -0300

    feat: __init__ module typed

--- stubs/Scrapy/scrapy/__init__.pyi
+++ stubs/Scrapy/scrapy/__init__.pyi
@@ -1,14 +1,12 @@
 def __getattr__(name: str): ...  # incomplete
 
-from _typeshed import Incomplete
-
 from scrapy.http import FormRequest as FormRequest, Request as Request
 from scrapy.item import Field as Field, Item as Item
 from scrapy.selector import Selector as Selector
 from scrapy.spiders import Spider as Spider
 
-version_info: Incomplete
-twisted_version: Incomplete
+version_info: tuple[int]
+twisted_version: tuple[int]
 
 # Names in __all__ with no definition:
 #   __version__
